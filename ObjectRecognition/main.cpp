9aa6b97ba5aa9c0ba055ea91c01adb81d601fbe4
Vamsi Mocherla <vamsimocherla@Vamsis-MacBook-Pro.local>
2014-03-16 20:34:46 -0500

    Initial Commit

---
+++ ObjectRecognition/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ObjectRecognition
+//
+//  Created by Vamsi Mocherla on 3/16/14.
+//  Copyright (c) 2014 VamsiMocherla. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
